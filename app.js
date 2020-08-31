dcb476ef760367b28881a80cf0788ad54e211b23
David Honório - UpHill <david.honorio@uphill.pt>
2020-08-31 08:06:52 +0100

    feat: add tiles generation on form submit

--- app.js
+++ app.js
@@ -51,8 +51,6 @@ const human = (function () {
 
 // Create Dino Compare Method 1
 // NOTE: Weight in JSON file is in lbs, height in inches. 
-    // NOTE: Weight in JSON file is in lbs, height in inches. 
-// NOTE: Weight in JSON file is in lbs, height in inches. 
 
 Dino.prototype.compareWeight = function (weight) {
   if (this.weight > weight) {
@@ -70,11 +68,56 @@ Dino.prototype.compareWeight = function (weight) {
 // NOTE: Weight in JSON file is in lbs, height in inches.
 
 
-    // Generate Tiles for each Dino in Array
-  
-        // Add tiles to DOM
+// Generate Tiles for each Dino in Array
+// Add tiles to DOM
 
-    // Remove form from screen
+function generateTiles(dinosList) {
+  console.log(dinosList);
+  const generateDinoTile = (dino) => {
+    let dinoNode = document.createElement('div');
+    dinoNode.className = 'grid-item';
+    let dinoSpecies = document.createElement('h3');
+    dinoSpecies.appendChild(document.createTextNode(dino.species));
+    let dinoImg = document.createElement('img');
+    dinoImg.src = `/images/${dino.species.toLowerCase()}.png`;
+    let dinoFact = document.createElement('p');
+    dinoFact.appendChild(document.createTextNode(dino.fact));
+    dinoNode.appendChild(dinoSpecies);
+    dinoNode.appendChild(dinoImg);
+    dinoNode.appendChild(dinoFact);
+    return dinoNode;
+  }
+
+  let grid = document.getElementById('grid');
+  dinosList.forEach(dino => {
+    let dinoNode = generateDinoTile(dino);
+    grid.appendChild(dinoNode);
+  });
+  let form = document.getElementById('dino-compare');
+  form.style = "display:none;"
+}
+
+
+// Remove form from screen
 
 
 // On button click, prepare and display infographic
+
+const dinoCompare = (event) => {
+  event.preventDefault();
+  console.log(human.getInfo());
+  const humanInfo = human.getInfo();
+  let dinos = dinosList.map(dino => {
+    return {
+      ...dino,
+      fact1: dino.compareWeight(humanInfo.weight)
+    }
+  })
+  generateTiles(dinos);
+}
+
+const dinoForm = document.getElementById('dino-compare')
+dinoForm.addEventListener('submit', dinoCompare);
+
+
+
