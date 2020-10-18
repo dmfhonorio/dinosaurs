835d39cbc7cc11c24f507bbc050ed92e40e4a241
David Honório - UpHill <david.honorio@uphill.pt>
2020-10-18 17:22:03 +0100

    refacor: add listener iife

--- app.js
+++ app.js
@@ -63,6 +63,7 @@ Dino.prototype.compareDiet = function (diet) {
     }
   }
 }
+
 let dinosList;
 fetch('dino.json')
   .then(res => res.json())
@@ -104,56 +105,57 @@ const human = (function () {
   }
 })();
 
-// Generate Tiles for each Dino in Array
-// Add tiles to DOM
+(function addListeners() {
 
-function generateTiles(dinos) {
-  const generateTileNode = (name, imgName, description = null) => {
-    let node = document.createElement('div');
-    node.className = 'grid-item';
-    let nodeName = document.createElement('h3');
-    nodeName.appendChild(document.createTextNode(name));
-    node.appendChild(nodeName);
-    let nodeImg = document.createElement('img');
-    nodeImg.src = `/images/${imgName}.png`;
-    node.appendChild(nodeImg);
-    if (description) {
-      let nodeDescription = document.createElement('p');
-      nodeDescription.appendChild(document.createTextNode(description));
-      node.appendChild(nodeDescription);
+  // Generate Tiles for each Dino in Array
+  // Add tiles to DOM
+
+  function generateTiles(dinos) {
+    const generateTileNode = (name, imgName, description = null) => {
+      let node = document.createElement('div');
+      node.className = 'grid-item';
+      let nodeName = document.createElement('h3');
+      nodeName.appendChild(document.createTextNode(name));
+      node.appendChild(nodeName);
+      let nodeImg = document.createElement('img');
+      nodeImg.src = `/images/${imgName}.png`;
+      node.appendChild(nodeImg);
+      if (description) {
+        let nodeDescription = document.createElement('p');
+        nodeDescription.appendChild(document.createTextNode(description));
+        node.appendChild(nodeDescription);
+      }
+      return node;
     }
-    return node;
+    const generateDinoTile = (dino) => {
+      return generateTileNode(dino.species, dino.species.toLowerCase(), dino.fact);
+    }
+    const generateHumanTile = () => {
+      const { name } = human.getInfo();
+      return generateTileNode(name, 'human');
+    }
+    const dinoNodes = [];
+    dinos.forEach(dino => {
+      let dinoNode = generateDinoTile(dino);
+      dinoNodes.push(dinoNode);
+    });
+    const middleIndex = Math.floor(dinos.length / 2);
+    const humanNode = generateHumanTile();
+    dinoNodes.splice(middleIndex, 0, humanNode)
+    removeForm();
+    let grid = document.getElementById('grid');
+    dinoNodes.forEach(node => {
+      grid.appendChild(node);
+    })
   }
-  const generateDinoTile = (dino) => {
-    return generateTileNode(dino.species, dino.species.toLowerCase(), dino.fact);
-  }
-  const generateHumanTile = () => {
-    const { name } = human.getInfo();
-    return generateTileNode(name, 'human');
-  }
-  const dinoNodes = [];
-  dinos.forEach(dino => {
-    let dinoNode = generateDinoTile(dino);
-    dinoNodes.push(dinoNode);
-  });
-  const middleIndex = Math.floor(dinos.length / 2);
-  const humanNode = generateHumanTile();
-  dinoNodes.splice(middleIndex, 0, humanNode)
-  removeForm();
-  let grid = document.getElementById('grid');
-  dinoNodes.forEach(node => {
-    grid.appendChild(node);
-  })
-}
 
 
-// Remove form from screen
-const removeForm = () => {
-  let form = document.getElementById('dino-compare');
-  form.style = "display:none;"
-}
-
-(function addListeners(){
+  // Remove form from screen
+  const removeForm = () => {
+    let form = document.getElementById('dino-compare');
+    form.style = "display:none;"
+  }
+  
   // On button click, prepare and display infographic
   const dinoForm = document.getElementById('dino-compare');
   dinoForm.addEventListener('submit', (event) => {
@@ -162,9 +164,12 @@ const removeForm = () => {
     let dinos = dinosList.map(dino => {
       return {
         ...dino,
-        fact1: dino.compareWeight(humanInfo.weight),
-        fact2: dino.compareHeight(humanInfo.height),
-        fact3: dino.compareDiet(humanInfo.diet)
+        facts: [
+          dino.fact,
+          dino.compareWeight(humanInfo.weight),
+          dino.compareHeight(humanInfo.height),
+          dino.compareDiet(humanInfo.diet)
+        ]
       }
     })
     generateTiles(dinos);
